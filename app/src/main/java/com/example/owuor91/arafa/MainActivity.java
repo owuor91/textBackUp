f4cc2c480cd5c06b4cb0ab291442658b4868b8f8
owuor91 <owuor91@gmail.com>
2015-11-05 18:14:33 +0300

    Add SMSData class

--- app/src/main/java/com/example/owuor91/arafa/MainActivity.java
+++ app/src/main/java/com/example/owuor91/arafa/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.owuor91.arafa;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String msgData;
+    TextView txtsms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtsms= (TextView)findViewById(R.id.txtsms);
+
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null,null,null);
+        if(cursor.moveToFirst()){
+            do {
+                msgData = "";
+                for (int idx=0; idx<cursor.getColumnCount(); idx++){
+                    msgData += ""+cursor.getColumnName(idx)+":"+cursor.getString(idx);
+                }
+
+            }
+            while (cursor.moveToNext());
+        }
+        else {
+            Toast.makeText(this, "You have no messages", Toast.LENGTH_SHORT).show();
+        }
+
+        txtsms.setText(msgData);
     }
 
     @Override
@@ -25,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         int id = item.getItemId();
 
-        
+
         if (id == R.id.action_settings) {
             return true;
         }
---
+++ app/src/main/java/com/example/owuor91/arafa/SMSData.java
@@ -0,0 +1,23 @@
+package com.example.owuor91.arafa;
+
+
+public class SMSData {
+    private String number;
+    private String body;
+
+    public  String getNumber(){
+        return number;
+    }
+
+    public void setNumber(String number){
+        this.number=number;
+    }
+
+    public String getBody(){
+        return body;
+    }
+
+    public void setBody(String body){
+        this.body = body;
+    }
+}
