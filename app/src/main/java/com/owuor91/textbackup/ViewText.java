bfc3fb55c8ff326d645860acd1cb461d8f3e5fc1
owuor91 <owuor91@gmail.com>
2015-11-14 13:36:36 +0300

    clean up teh codes

--- app/src/main/java/com/owuor91/textbackup/ViewText.java
+++ app/src/main/java/com/owuor91/textbackup/ViewText.java
@@ -1,35 +1,27 @@
 package com.owuor91.textbackup;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.content.IntentSender;
-import android.graphics.Bitmap;
+
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.drive.DriveApi;
-import com.owuor91.textbackup.R;
 
-import java.io.BufferedReader;
+
 import java.io.ByteArrayOutputStream;
-import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.io.OutputStream;
 
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.ResultCallback;
@@ -40,18 +32,12 @@ import com.google.android.gms.drive.MetadataChangeSet;
 public class ViewText extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener{
 
     private static final String TAG = "Save file ";
-    private static final int REQUEST_CODE_CAPTURE_IMAGE = 1;
-    private static final int REQUEST_CODE_CREATOR = 2;
     private static final int REQUEST_CODE_RESOLUTION = 3;
 
     private GoogleApiClient googleApiClient;
-    private Bitmap bitmapToSave;
-    private File txtFile;
 
     private void saveFileToDrive(){
         Log.i(TAG, "Creating new contents.");
-        final Bitmap image = bitmapToSave;
-        final File file = txtFile;
 
         Drive.DriveApi.newDriveContents(googleApiClient)
                 .setResultCallback(new ResultCallback<DriveContentsResult>() {
@@ -64,9 +50,6 @@ public class ViewText extends AppCompatActivity implements ConnectionCallbacks, 
 
                         Log.i(TAG,"New contents created");
                         OutputStream outputStream = driveContentsResult.getDriveContents().getOutputStream();
-
-                        /*ByteArrayOutputStream bitmapStream = new ByteArrayOutputStream();
-                        image.compress(Bitmap.CompressFormat.PNG, 100,bitmapStream);*/
 
                         try {
                             FileInputStream fileInputStream = getBaseContext().openFileInput("sms_file.txt");
@@ -87,19 +70,6 @@ public class ViewText extends AppCompatActivity implements ConnectionCallbacks, 
                                 .setMimeType("text/plain")
                                 .setTitle("txtfile")
                                 .build();
-
-                        /*IntentSender intentSender = Drive.DriveApi.newCreateFileActivityBuilder()
-                                .setInitialMetadata(metadataChangeSet)
-                                .setInitialDriveContents(driveContentsResult.getDriveContents())
-                                .build(googleApiClient);
-
-
-                        try {
-                            startIntentSenderForResult(intentSender, REQUEST_CODE_CREATOR, null, 0,0,0);
-                        }
-                        catch (IntentSender.SendIntentException e){
-                            Log.i(TAG,"Failed to launch file chooser");
-                        }*/
 
                         Drive.DriveApi.getRootFolder(googleApiClient)
                                 .createFile(googleApiClient, metadataChangeSet, driveContentsResult.getDriveContents());
@@ -129,25 +99,6 @@ public class ViewText extends AppCompatActivity implements ConnectionCallbacks, 
         super.onPause();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode){
-            case REQUEST_CODE_CAPTURE_IMAGE:
-                if (resultCode== Activity.RESULT_OK){
-                    bitmapToSave = (Bitmap) data.getExtras().get("data");
-                }
-                break;
-            case REQUEST_CODE_CREATOR:
-                if (resultCode==RESULT_OK){
-                    Log.i(TAG, "Image successfully saved");
-                    bitmapToSave = null;
-                    Intent intent = new Intent(this, MainActivity.class);
-                    startActivity(intent);
-                    //startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE),REQUEST_CODE_CAPTURE_IMAGE);
-                }
-                break;
-        }
-    }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
@@ -168,10 +119,7 @@ public class ViewText extends AppCompatActivity implements ConnectionCallbacks, 
     @Override
     public void onConnected(Bundle bundle) {
         Log.i(TAG, "API client connected");
-        /*if (bitmapToSave==null){
-            startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE),REQUEST_CODE_CAPTURE_IMAGE);
-            return;
-        }*/
+
         saveFileToDrive();
     }
 
@@ -182,44 +130,3 @@ public class ViewText extends AppCompatActivity implements ConnectionCallbacks, 
 }
 
 
-/*
-
-@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_text);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-    }
-
-public String goodtext = "Empty line";
-FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-fab.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show();
-        }
-        });
-
-        TextView textView= (TextView)findViewById(R.id.smsbody);
-
-
-        try {
-        FileInputStream fileInputStream = getBaseContext().openFileInput("sms_file.txt");
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        StringBuilder stringBuilder = new StringBuilder();
-        String line;
-        while ((line=bufferedReader.readLine())!=null){
-        stringBuilder.append(line);
-        goodtext+=line+"\n"+"\n";
-        }
-        }
-        catch (Exception e){
-        e.printStackTrace();
-        }
-
-        textView.setText(goodtext);*/
