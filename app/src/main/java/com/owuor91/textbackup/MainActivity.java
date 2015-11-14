22e76270b6410a74c4442c3a36df04aeb98904f6
owuor91 <owuor91@gmail.com>
2015-11-14 13:52:12 +0300

    move google drive manenos to main activity

--- app/src/main/java/com/owuor91/textbackup/MainActivity.java
+++ app/src/main/java/com/owuor91/textbackup/MainActivity.java
@@ -4,11 +4,13 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,17 +18,37 @@ import android.widget.ListView;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity{
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi.DriveContentsResult;
+import com.google.android.gms.drive.MetadataChangeSet;
+
+
+public class MainActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener{
     public String body, address, row, content="";
     public long datetime=0;
     ListView lvSMS;
     public Context context;
     SwipeRefreshLayout swipe;
+
+    private static final String TAG = "Save file ";
+    private static final int REQUEST_CODE_RESOLUTION = 3;
+
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +65,46 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
+
+    private void saveFileToDrive(){
+        Log.i(TAG, "Creating new contents.");
+
+        Drive.DriveApi.newDriveContents(googleApiClient)
+                .setResultCallback(new ResultCallback<DriveContentsResult>() {
+                    @Override
+                    public void onResult(DriveContentsResult driveContentsResult) {
+                        if (!driveContentsResult.getStatus().isSuccess()) {
+                            Log.i(TAG, "Failed to create new contents");
+                            return;
+                        }
+
+                        Log.i(TAG, "New contents created");
+                        OutputStream outputStream = driveContentsResult.getDriveContents().getOutputStream();
+
+                        try {
+                            FileInputStream fileInputStream = getBaseContext().openFileInput("sms_file.txt");
+                            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                            int nRead;
+                            byte[] data = new byte[16384];
+                            while ((nRead = fileInputStream.read(data, 0, data.length)) != -1) {
+                                byteArrayOutputStream.write(data, 0, nRead);
+                            }
+                            byteArrayOutputStream.flush();
+                            outputStream.write(byteArrayOutputStream.toByteArray());
+                        } catch (IOException io) {
+                            Log.i(TAG, "Unable to write file contents");
+                        }
+
+                        MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
+                                .setMimeType("text/plain")
+                                .setTitle("txtfile")
+                                .build();
+
+                        Drive.DriveApi.getRootFolder(googleApiClient)
+                                .createFile(googleApiClient, metadataChangeSet, driveContentsResult.getDriveContents());
+                    }
+                });
+    }
 
     public void swipeLayout(){
         swipe = (SwipeRefreshLayout)findViewById(R.id.swipelayout);
@@ -84,8 +146,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 SMSData contact = (SMSData) lvSMS.getItemAtPosition(position);
-                Intent intent = new Intent(context,ViewText.class);
-                startActivity(intent);
+
             }
         });
 
@@ -130,5 +191,56 @@ public class MainActivity extends AppCompatActivity{
         toolbar.setTitleTextColor(0xFFFFFFFF);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Messages");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (googleApiClient==null){
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        if (googleApiClient != null){
+            googleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "GoogleApiClient connection failed: "+ connectionResult.toString());
+        if (!connectionResult.hasResolution()){
+            GoogleApiAvailability.getInstance().getErrorDialog(this, connectionResult.getErrorCode(),0).show();
+            return;
+        }
+
+        try {
+            connectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        }
+        catch (IntentSender.SendIntentException e){
+            Log.e(TAG, "Exception while starting resolution activity", e);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "API client connected");
+
+        saveFileToDrive();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleAPIClient connection suspended");
     }
 }
